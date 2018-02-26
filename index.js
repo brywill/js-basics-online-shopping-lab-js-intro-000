bf53f2315cbc917d7cc6b3aec55ee755d15ecb9b
Bryan Williams <brywilldev@gmail.com>
2018-02-26 00:42:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,11 @@ function addToCart(item) {
 }
 
 function viewCart() {
-  // write your code here
+  if (cart.length === 0) {
+    return "Your shopping cart is empty."
+  } else if (cart.length === 1) {
+    return `In your cart, you have ${cart[0].[key]}s at $${cart[0].[value]}`
+  }
 }
 
 function total() {
